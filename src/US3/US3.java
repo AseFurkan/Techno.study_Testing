c181b45937b5e4cc05307a28e6ac98addec8356f
yoral8 <ysrorl@hotmail.com>
2023-09-03 15:07:20 +0300

    işlem tamam gibi

--- src/US3/US3.java
+++ src/US3/US3.java
@@ -1,4 +1,60 @@
 package US3;
 
-public class US3 {
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class US3 extends BaseDriver {
+    @Test
+    public void test1(){
+        driver.get("https://techno.study/tr");
+        WebElement başvur=driver.findElement(By.linkText("BAŞVUR"));
+        başvur.click();
+
+        WebElement adsoyad= driver.findElement(By.xpath("//input[@name='name']"));
+        adsoyad.sendKeys("test Ali KARACA");
+
+        WebElement email = driver.findElement(By.xpath("//input[@name='email']"));
+        email.sendKeys("testalikaraca@gmail.com");
+
+        WebElement gönder = driver.findElement(By.xpath("//button[@type='submit']"));
+        Actions aksiyon = new Actions(driver);
+        aksiyon.moveToElement(gönder).build().perform();
+
+        WebElement telefon= driver.findElement(By.xpath("//span[@class='t-input-phonemask__select-triangle']"));
+        telefon.click();
+
+        WebElement andor= driver.findElement(By.cssSelector("[id='t-phonemask_ad']"));
+        andor.click();
+
+        WebElement telno= driver.findElement(By.xpath("//input[@type='tel']"));
+        telno.sendKeys("123456");
+
+        WebElement ülke = driver.findElement(By.xpath("//option[@value='Andorra']"));
+        ülke.click();
+
+        MyFunc.bekle(2);
+        WebElement kurs= driver.findElement(By.xpath("//select[@name='course']"));
+        kurs.click();
+
+        WebElement sdet = driver.findElement(By.xpath("//option[@value='SDET Türkçe']"));
+        sdet.click();
+
+        WebElement ref = driver.findElement(By.xpath("//select[@name='survey']"));
+        ref.click();
+
+        WebElement başka= driver.findElement(By.xpath("//option[@value='Başka']"));
+        başka.click();
+
+        WebElement şartlar = driver.findElement(By.xpath("//div[@class='t-checkbox__indicator']"));
+        şartlar.click();
+
+        gönder.click();
+    BekleVeKapat();
+    }
+
+
 }
--- src/US4/US4.java
+++ src/US4/US4.java
@@ -1,4 +1,68 @@
 package US4;
 
-public class US4 {
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class US4 extends BaseDriver {
+    @Test
+    public void test4(){
+        driver.navigate().to("https://techno.study/tr");
+        WebElement kurslar= driver.findElement(By.xpath("//a[text()='Kurslar']"));
+        kurslar.click();
+
+        WebElement sdet= driver.findElement(By.xpath("//div[@class='t966__menu-item-title t-name']"));
+        sdet.click();
+        MyFunc.bekle(2);
+
+        driver.navigate().back();
+        MyFunc.bekle(2);
+
+        kurslar.click();
+
+        WebElement android=driver.findElement(By.xpath("//div[text()='Android uygulama geliştirme']"));
+        android.click();
+        MyFunc.bekle(2);
+
+        driver.navigate().back();
+
+        kurslar.click();
+
+        WebElement veribilimi=driver.findElement(By.xpath("//div[text()='Veri bilimi']"));
+        veribilimi.click();
+        MyFunc.bekle(2);
+
+        driver.navigate().back();
+
+        kurslar.click();
+
+        WebElement job=driver.findElement(By.xpath("//div[text()='Job Center & Arbeitsamt']"));
+        job.click();
+        MyFunc.bekle(2);
+
+        driver.navigate().back();
+
+        WebElement master= driver.findElement(By.linkText("Master's Program"));
+        master.click();
+
+        driver.navigate().back();
+
+        BekleVeKapat();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
