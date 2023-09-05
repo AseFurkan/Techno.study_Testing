8ef2289bc00f69e20963b98ec1b7e7188f7cede5
mustafayumrutepe <mstfyumrutepe@gmail.com>
2023-09-05 17:41:38 +0300

    US8 bitti

--- src/US8/US8.java
+++ src/US8/US8.java
@@ -1,4 +1,23 @@
 package US8;
 
-public class US8 {
+import Utility.BaseDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class US8 extends BaseDriver {
+    @Test
+    public void test1() {
+        driver.get("https://techno.study/tr");
+
+        WebElement varMi = driver.findElement(By.xpath("//span[@class='t-checkbox__labeltext']"));
+        varMi.getText();
+        Assert.assertTrue(varMi.getText().contains("Şartları okudum ve kabul ettim."));
+
+        WebElement termsOfUse = driver.findElement(By.xpath("//span[text()='Şartları']"));
+        termsOfUse.click();
+
+        BekleVeKapat();
+    }
 }
